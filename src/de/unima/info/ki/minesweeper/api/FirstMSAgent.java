335a4301236f58462806b10226c953858882037e
Jonas Rudi <jonas.rudi@hotmail.com>
2017-11-08 22:02:47 +0100

    added FirstMSAgent

--- src/de/unima/info/ki/minesweeper/api/FirstMSAgent.java
+++ src/de/unima/info/ki/minesweeper/api/FirstMSAgent.java
@@ -3,13 +3,18 @@ package de.unima.info.ki.minesweeper.api;
 public class FirstMSAgent extends MSAgent{
     private boolean displayActivated = false;
     private boolean firstDecision = true;
-
+    private int[][] progress;
     @Override
     public boolean solve() {
         int numOfRows = this.field.getNumOfRows();
         int numOfCols = this.field.getNumOfCols();
         int x, y, feedback;
-
+        this.progress = new int[numOfRows][numOfCols];
+        for (int i = 0; i < numOfRows; i++) {
+            for (int j = 0; j < numOfCols; j++) {
+                progress[i][j] = -1;
+            }
+        }
         do {
             if (displayActivated) System.out.println(field);
             if (firstDecision) {
@@ -18,13 +23,23 @@ public class FirstMSAgent extends MSAgent{
                 firstDecision = false;
             }
             else {
+
                 //TODO HIER ALGORITHMUS
                 x = numOfCols;
                 y = numOfRows;
+                /*
+                vielleicht kann man, wenn man eine 0 zurückbekommt, eine Queue erstellen,
+                in der die uliegenden Felder eingetragen werden. Und solange Elemente in der Queue sind,
+                werden diese zuerst abgearbeitet.
+                Wird ja theoretisch bei Microsoft MineSweeper auch so gemacht, wenn man ein Feld mit ner 0 trifft,
+                werden alle leeren Felder ausßenrum auch aufgedeckt.
+                 */
             }
 
             if (displayActivated) System.out.println("Uncovering (" + x + "," + y + ")");
             feedback = field.uncover(x,y);
+
+            progress[x][y]=feedback;
 
         } while(feedback >= 0 && !field.solved());
 
@@ -49,5 +64,7 @@ public class FirstMSAgent extends MSAgent{
         this.displayActivated = false;
     }
 
+
+    }
 }
 
